5158cfbcc2532c4153742d58a72f020465d81cbb
VictoriaBil <108450450+VictoriaBil@users.noreply.github.com>
2022-11-30 15:52:12 +0000

    Deploying to gh-pages from @ VictoriaBil/goit-js-hw-08@b255742d30872a89b53c898ed3781dbcb6a928ab 🚀

---
+++ 03-feedback.c0a2da5f.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".feedback-form");var e=document.querySelector("input"),t=document.querySelector("textarea");e.addEventListener("input",(function(e){console.log(e.currentTarger.value)})),t.addEventListener("textarea",onUserMessage)}();
+//# sourceMappingURL=03-feedback.c0a2da5f.js.map
---
+++ 03-feedback.c0d5bf9c.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form");const e=document.querySelector("input"),t=document.querySelector("textarea");e.addEventListener("input",(function(e){console.log(e.currentTarger.value)})),t.addEventListener("textarea",onUserMessage);
+//# sourceMappingURL=03-feedback.c0d5bf9c.js.map
