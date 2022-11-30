4fe3817dcd9c89b07f6352957f8459cf553c4922
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2022-11-30 17:31:51 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', function onPlay(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+});
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
