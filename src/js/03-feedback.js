b255742d30872a89b53c898ed3781dbcb6a928ab
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2022-11-30 17:51:45 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,10 @@
+const form = document.querySelector('.feedback-form');
+const userEmail = document.querySelector('input');
+const userMessege = document.querySelector('textarea');
 
+userEmail.addEventListener('input', onEmailInput);
+userMessege.addEventListener('textarea', onUserMessage);
+
+function onEmailInput(event) {
+  console.log(event.currentTarger.value);
+}
